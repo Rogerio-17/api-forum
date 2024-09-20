2fddea09c58f54e3eb967816524ef0bd3290dd10
Rogerio-17 <rogeriojmf10@gmail.com>
2024-09-19 22:29:58 -0300

    feat: adiciona funcionalidade de delete de uma question

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -5,6 +5,18 @@ import { QuestionsRepository } from "@/domain/repositories/questions-repository"
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = [];
 
+  async findById(id: string) {
+    const question = this.items.find(
+      (question) => question.id.toString() === id
+    );
+
+    if (!question) {
+      return null;
+    }
+
+    return question;
+  }
+
   async findBySlug(slug: string) {
     const question = this.items.find(
       (question) => question.slug.value === slug
@@ -19,5 +31,11 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question) {
     this.items.push(question);
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id);
+
+    this.items.splice(itemIndex, 1);
   }
 }
